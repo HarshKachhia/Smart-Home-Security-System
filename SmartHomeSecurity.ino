5b8bd678e9d6573de9be50fc97179236bc75cc58
Harsh Kachhia <96410794+HarshKachhia@users.noreply.github.com>
2023-04-10 22:45:12 -0400

    Update SmartHomeSecurity.ino

--- SmartHomeSecurity.ino
+++ SmartHomeSecurity.ino
@@ -1,7 +1,9 @@
+#include <Adafruit_NeoPixel.h>
+
 // Pinout definitions
 int PSD_PIN = A5;
 int PHOTOC_PIN = A0;
-int LED_PIN = 13;
+int NEOPIXEL_PIN = 13;
 int SPEAKER_PIN = 10;
 int ALARM_DELAY = 5000;
 int IR_PIN = 4;
@@ -12,17 +14,52 @@ int SENSOR_PIN = 2;
 int lastIrValue = 0;
 boolean sensorValue = 0;
 
+// Define NeoPixel stick object
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+// Function to turn on speaker sound and NeoPixel with given color
+void turnOnAlarm(int r, int g, int b) {
+  // Turn on NeoPixel
+  strip.setPixelColor(0, r, g, b);
+   strip.setPixelColor(1, r, g, b);
+    strip.setPixelColor(2, r, g, b);
+     strip.setPixelColor(3, r, g, b);
+      strip.setPixelColor(4, r, g, b);
+       strip.setPixelColor(5, r, g, b);
+        strip.setPixelColor(6, r, g, b);
+
+  strip.show();
+
+  // Turn on speaker
+  tone(SPEAKER_PIN, 1000);
+  delay(ALARM_DELAY);
+  noTone(SPEAKER_PIN);
+
+  // Turn off NeoPixel
+  strip.setPixelColor(0, 0, 0, 0);
+  strip.setPixelColor(1, 0, 0, 0);
+  strip.setPixelColor(2, 0, 0, 0);
+  strip.setPixelColor(3, 0, 0, 0);
+  strip.setPixelColor(4, 0, 0, 0);
+  strip.setPixelColor(5, 0, 0, 0);
+  strip.setPixelColor(6, 0, 0, 0);
+  strip.show();
+}
+
 void setup() {
   // Initialize serial communication
   Serial.begin(9600);
 
   // Set pin modes
-  pinMode(LED_PIN, OUTPUT);
   pinMode(SPEAKER_PIN, OUTPUT);
   pinMode(IR_PIN, INPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(TOUCH_PIN, INPUT);
   pinMode(SENSOR_PIN, INPUT);
+
+  // Initialize NeoPixel
+  strip.begin();
+  strip.setBrightness(50);
 }
 
 void loop() {
@@ -37,34 +74,32 @@ void loop() {
   Serial.println(lightLevel);
 
   // Check if an object is detected within a certain distance
-  if (distance < 300) {
-    // Trigger alarm and turn on LED
-    digitalWrite(LED_PIN, HIGH);
-    tone(SPEAKER_PIN, 1000);
-    delay(ALARM_DELAY);
-    noTone(SPEAKER_PIN);
-    digitalWrite(LED_PIN, LOW);
+  if (distance < 200) {
+    // Trigger alarm and turn on NeoPixel
+   Serial.println("PSD Triggered: Intruder Alert");
+    turnOnAlarm(255, 0, 0); // Red
   }
 
   // Check for changes in ambient light
   if (lightLevel > 300) {
-    // Trigger alarm and turn on LED
-    digitalWrite(LED_PIN, HIGH);
-    tone(SPEAKER_PIN, 1000);
-    delay(ALARM_DELAY);
-    noTone(SPERAKE_PIN);
-    digitalWrite(LED_PIN, LOW);
+    // Trigger alarm and turn on NeoPixel
+    Serial.println("Photcell Sensor Triggered: Intruder Alert");
+    turnOnAlarm(255, 255, 255); // White
   }
-
+  //if button is pressed
+ if (digitalRead(BUTTON_PIN) == 1)
+  {
+    Serial.println("Passcode Inputted: Welcome Homeowner");
+    delay(1000);
+  }
   // Check if button is not pressed
   if (digitalRead(BUTTON_PIN) != 1) {
     // Check if IR sensor value changes
     if (digitalRead(IR_PIN) != lastIrValue) {
       // Turn on speaker for 1 second
-      digitalWrite(SPEAKER_PIN, HIGH);
-      delay(1000);
-      digitalWrite(SPEAKER_PIN, LOW);
-      
+      Serial.print("IR sensor Triggered: Intruder Alert");
+       turnOnAlarm(0, 255, 0); // Green
+
       // Print out IR sensor value
       Serial.print("IR sensor value: ");
       Serial.println(digitalRead(IR_PIN));
@@ -77,31 +112,26 @@ void loop() {
   // Check if touch sensor is triggered
   if (digitalRead(TOUCH_PIN) == HIGH) {
     // Turn on speaker
-    digitalWrite(SPEAKER_PIN, HIGH);
-    delay(1500);
-    // Turn off speaker
-    digitalWrite(SPEAKER_PIN, LOW);
-    delay(1500);
-    
+   Serial.println("Touch Sensor Triggered: Back Door Break-in Detected");
+   turnOnAlarm(0, 255, 255); //Cyan color
+
     // Print out touch sensor value
     Serial.print("Touch sensor value: ");
     Serial.println(digitalRead(TOUCH_PIN));
   }
 
-  // Read generic sensor value
+  // Read sound sensor value
   sensorValue = digitalRead(SENSOR_PIN);
-  Serial.println(sensorValue);
+  Serial.print("Sound Sensor Value: ");
+ Serial.println(sensorValue);
 
-  // Check if generic sensor value is HIGH
+  // Check if sound sensor value is HIGH
   if (sensorValue == HIGH) {
-    digitalWrite(LED_PIN, HIGH);
-    tone(SPEAKER_PIN, 2000);
-    delay(1000);
-    noTone(SPEAKER_PIN);
-    digitalWrite(LED_PIN, LOW);
+    Serial.println("Sound Sensor Triggered: Window Break-in Detected");
+    turnOnAlarm(255, 0, 255); //magenta color
   }
   else {
-    digitalWrite(LED_PIN, LOW);
+    digitalWrite(NEOPIXEL_PIN, LOW);
     digitalWrite(SPEAKER_PIN, LOW);
   }
 
